d1c313c91832583e4ea9d01c326cca3b2957c198
Vijay Kumar <vijay.948319@gmail.com>
2026-01-16 00:55:38 +0530

    updated main.tsx & main.scss

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -12,16 +12,16 @@ function Main() {
           <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
         </div>
         <div className="content">
-          <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
-          </div>
-          <h1>Yuji Sato</h1>
-          <p>Full Stack Engineer</p>
+          {/* <div className="social_icons">
+            <a href="" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+          </div> */}
+          <h1>Vijay Kumar</h1>
+          <p>DevOps Engineer</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/vijay-kumar95" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/vijaykumar12s/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
       </div>
