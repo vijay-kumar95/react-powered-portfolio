a17a2e0ea82ca92b3920c493611c67d604db888c
Vijay Kumar <vijay.948319@gmail.com>
2026-03-29 13:33:47 +0530

    ci.yml updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+// import GitHubIcon from '@mui/icons-material/GitHub';
+// import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Footer.scss'
 
 function Footer() {
