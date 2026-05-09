6fb40beda3143352842858c405f4c4f77b329502
Vijay Kumar <vijay.948319@gmail.com>
2026-05-09 13:12:09 +0530

    main.tsx updated for testing

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -19,7 +19,7 @@ function Main() {
             <a href="" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div> */}
           <h1>Vijay Kumar</h1>
-          <p>DevOps/SRE/Platform Engineer</p>
+          <p>Platform Engineer</p>
 
           <div className="mobile_social_icons">
             <a href="https://github.com/vijay-kumar95" target="_blank" rel="noreferrer"><GitHubIcon/></a>
