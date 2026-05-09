32e1a5b352d47727567776cb177ba3f8e3a097c8
Vijay Kumar <vijay.948319@gmail.com>
2026-05-09 22:17:57 +0530

    footer updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ import '../assets/styles/Footer.scss'
 function Footer() {
   return (
     <footer className="footer">
-    <p>Portfolio originally built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> & it's redesigned for personal use by <a href="https://github.com/vijay-kumar95" target="_blank" rel="noreferrer">Vijay Kumar!</a></p>
+    <p>Portfolio originally built by????????????? <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> & it's redesigned for personal use by <a href="https://github.com/vijay-kumar95" target="_blank" rel="noreferrer">Vijay Kumar!</a></p>
     </footer>
   );
 }
